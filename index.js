3f5cf522828ca52a5da19cb65cbbe8f0fee1f99c
NutronStar45 <vam3000.chen@gmail.com>
2021-06-22 08:34:51 +0800

    Patch 1.0.6

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // Log every available pages
 const pages = [
 	{
+		title: '404 Not Found',
+		relation: 'HTTP404',
+		path: {
+			relativePath: null,
+			relativeIndexPath: '/http404.html',
+			relativeStylesheetPath: '/',
+			relativeStylesheetIndexPath: '/index.css',
+			relativeScriptPath: '/',
+			relativeScriptIndexPath: '/index.js'
+		},
+		attributes: {
+			toolbar: {
+				enabled: true,
+				class: 'toolbar',
+				id: 'toolbarMeta'
+			}
+		}
+	},
+	{
 		title: 'NutronStar45\'s Work',
 		relation: 'Home Page',
 		path: {
@@ -44,21 +63,24 @@ const pages = [
 window.onload = () => {
 	pages.forEach(element => {
 		// If the page has toolbar enabled
-		if (document.getElementById('pageTitle').innerHTML == element.title &&
-			element.attributes.toolbar.enabled) {
-			document.getElementById('toolbarMeta').innerHTML = `
+		if (document.title == element.title &&
+			element.attributes.toolbar.enabled)
+			document.getElementById(element.attributes.toolbar.id).innerHTML = `
 				<!-- Homepage icon -->
 				<a xlink:href="/">
 					<rect width="50" height="50" style="fill:#fff" />
-					<path d="M5 25 l20 -20 l20 20" stroke="#7f7f7f" stroke-width="3" fill="none" />
+					<polyline points="5,25 25,5 45,25" style="fill:none;stroke:#7f7f7f;stroke-width:3;" />
 					<rect y="25" width="50" height="25" style="fill:#fff" />
-					<path d="M12 25 l0 20 l26 0 l0 -20" stroke="#7f7f7f" stroke-width="3" fill="none" />
+					<polyline points="12,25 12,45 38,45 38,25" style="fill:none;stroke:#7f7f7f;stroke-width:3;" />
 				</a>
 				<!-- Projects -->
 				<a xlink:href="/projs">
 					<text x="100" y="30" fill="#000">Projects</text>
 				</a>
 			`;
-		}
+
+		// File missing location on HTTP404
+		if (element.relation == 'HTTP404')
+			document.getElementById('pageSubtitle').innerHTML = `File or Site missing at <code>${ window.location.pathname }</code>`;
 	});
 }
