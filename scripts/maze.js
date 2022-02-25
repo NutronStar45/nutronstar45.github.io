63d300321a8669739004ccc8cdca5ec2a7bbdd40
NutronStar45 <vam3000.chen@gmail.com>
2022-02-25 23:19:34 +0800

    Fix 2.4.4
    Tweaked links
    First version of Maze Generator Online
    Maze scripts test

---
+++ scripts/maze.js
@@ -0,0 +1,15 @@
+$(function() {
+
+  $('#maze-width').change(function() {
+    console.log('Maze width has been changed to ' + $(this).val())
+  });
+
+  $('#maze-height').change(function() {
+    console.log('Maze height has been changed to ' + $(this).val())
+  });
+
+  $('#maze-enable-solving').change(function() {
+    console.log('Maze solving has been changed to ' + $(this).val())
+  });
+
+});
