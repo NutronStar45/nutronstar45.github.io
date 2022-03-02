4a9f35e1b1c40da3decff4ab777ba62197af67a3
NutronStar45 <vam3000.chen@gmail.com>
2022-03-02 18:26:14 +0800

    Patch 2.8
    Tweaked `projs/maze`
    Maze scripts test
    Tweaked CSS

--- scripts/maze.js
+++ scripts/maze.js
@@ -1,12 +1,5 @@
-$(function() {
-
-  $('#maze-solver-options').hide();
-
-  $('#maze-enable-solving').change(function() {
-    $('#maze-solver-options').toggle();
-  });
-
-  $('#maze-gen-maze').click(function() {
+function generate(solving) {
+  if (solving) {
     $(this).next().html(`
 Width: ${+$('#maze-width').val()} <br>
 Height: ${+$('#maze-height').val()} <br>
@@ -15,6 +8,30 @@ Start Y: ${+$('#maze-start-y').val()} <br>
 End X: ${+$('#maze-end-x').val()} <br>
 End Y: ${+$('#maze-end-y').val()}
     `);
+  } else {
+    $(this).next().html(`
+Width: ${+$('#maze-width').val()} <br>
+Height: ${+$('#maze-height').val()}
+    `);
+  }
+}
+
+
+$(function() {
+
+  $('#maze-solver-options').hide();
+
+  $('#maze-enable-solving').change(function() {
+    $('#maze-solver-options').toggle();
+    if ($(this).is(':checked')) {
+      $('#maze-gen-solve').text('Generate & Solve');
+    } else {
+      $('#maze-gen-solve').text('Generate');
+    }
+  });
+
+  $('#maze-gen-solve').click(function() {
+    generate($('#maze-enable-solving').is(':checked'));
   });
 
 });
