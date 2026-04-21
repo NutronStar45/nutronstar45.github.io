76e2275b97c03b16ae1151bf2a7630ee89e32cf3
NutronStar45 <56185420+NutronStar45@users.noreply.github.com>
2026-04-21 22:08:13 +0800

    feat(Unicode converter): conversion to and from code points (decimal)

--- dist/unicode_converter/unicode_converter.mjs
+++ dist/unicode_converter/unicode_converter.mjs
@@ -1,28 +1,36 @@
 import $ from "jquery";
 const HEX_DIGIT_REGEX = /[0-9a-fA-F]/;
+const DEC_DIGIT_REGEX = /[0-9]/;
 /**
  * Throws an error if the given code point isn't valid.
+ * @param decimal Whether to display the code point in decimal in an error message. Defaults to displaying in hex.
  * @throws {TypeError} Thrown when the given code point is:
  * - not an integer,
  * - outside the valid range, or
  * - reserved for a surrogate.
  */
-function validateCodePoint(codePoint) {
+function validateCodePoint(codePoint, decimal = false) {
     if (!Number.isInteger(codePoint)) {
-        throw new TypeError(`Non-integer code point: ${codePoint}`);
+        throw new TypeError(`Non-integer code point (${codePoint})`);
     }
-    if (codePoint < 0 || codePoint > 0x10FFFF) {
-        let codePointDisplay = Math.abs(codePoint).toString(16).toUpperCase();
+    let codePointDisplay;
+    if (decimal) {
+        codePointDisplay = codePoint.toString();
+    }
+    else {
+        codePointDisplay = Math.abs(codePoint).toString(16).toUpperCase();
         if (codePoint >= 0) {
             codePointDisplay = "0x" + codePointDisplay.padStart(4, "0");
         }
         else {
             codePointDisplay = "-0x" + codePointDisplay;
         }
-        throw new TypeError(`Code point outside valid range: ${codePointDisplay}`);
+    }
+    if (codePoint < 0 || codePoint > 0x10FFFF) {
+        throw new TypeError(`Code point outside valid range (${codePointDisplay})`);
     }
     if (codePoint >= 0xD800 && codePoint <= 0xDFFF) {
-        throw new TypeError(`Surrogate code point: 0x${codePoint.toString(16).toUpperCase()}`);
+        throw new TypeError(`Surrogate code point (${codePointDisplay})`);
     }
 }
 /**
@@ -59,10 +67,10 @@ function fromCodePointsHex(str) {
             }
         }
         else if (HEX_DIGIT_REGEX.test(char)) {
-            if (partialCodePointHex.length === 6) {
+            partialCodePointHex += char.toUpperCase();
+            if (partialCodePointHex.length > 6) {
                 throw new TypeError(`Hex digit sequence longer than 6 digits (${partialCodePointHex})`);
             }
-            partialCodePointHex += char.toUpperCase();
         }
         else {
             throw new TypeError(`Encountered a character that is neither a hex digit nor a whitespace (\"${char}\")`);
@@ -71,6 +79,43 @@ function fromCodePointsHex(str) {
     if (partialCodePointHex !== "") {
         let codePoint = Number.parseInt(partialCodePointHex, 16);
         validateCodePoint(codePoint);
+        sequence.push(codePoint);
+    }
+    return sequence;
+}
+/**
+ * Converts the decimal representation of a code point sequence into the code point sequence.
+ * @throws {TypeError} Thrown when the given string:
+ * - contains a character that is neither a decimal digit nor a whitespace,
+ * - contains a decimal digit sequence with length greater than 7 digits,
+ * - contains a code point outside the valid range, or
+ * - contains a code point reserved for an surrogate.
+ */
+function fromCodePointsDec(str) {
+    let sequence = [];
+    let partialCodePointDec = ""; // A partially-built code point (in decimal representation)
+    for (const char of str) {
+        if (/\s/.test(char)) {
+            if (partialCodePointDec !== "") {
+                let codePoint = Number(partialCodePointDec);
+                validateCodePoint(codePoint, true);
+                sequence.push(codePoint);
+                partialCodePointDec = "";
+            }
+        }
+        else if (DEC_DIGIT_REGEX.test(char)) {
+            partialCodePointDec += char;
+            if (partialCodePointDec.length > 7) {
+                throw new TypeError(`Decimal digit sequence longer than 7 digits (${partialCodePointDec})`);
+            }
+        }
+        else {
+            throw new TypeError(`Encountered a character that is neither a decimal digit nor a whitespace (\"${char}\")`);
+        }
+    }
+    if (partialCodePointDec !== "") {
+        let codePoint = Number(partialCodePointDec);
+        validateCodePoint(codePoint, true);
         sequence.push(codePoint);
     }
     return sequence;
@@ -140,6 +185,23 @@ function toCodePointsHex(sequence) {
     return string;
 }
 /**
+ * Converts a code point sequence to its decimal representation.
+ * @throws {TypeError} Thrown when the given sequence contains a code point that is:
+ * - not an integer,
+ * - outside the valid range, or
+ * - reserved for a surrogate.
+ */
+function toCodePointsDec(sequence) {
+    let string = "";
+    for (const [i, codePoint] of sequence.entries()) {
+        validateCodePoint(codePoint);
+        if (i > 0)
+            string += " ";
+        string += codePoint.toString();
+    }
+    return string;
+}
+/**
  * Converts a code point sequence to UTF-32.
  * @throws {TypeError} Thrown when the given sequence contains a code point that is:
  * - not an integer,
@@ -159,14 +221,16 @@ function toUTF32Hex(sequence) {
 /** Clears messages */
 function clearMessages() {
     $("div#text-message").empty();
-    $("div#code-points-message").empty();
+    $("div#code-points-hex-message").empty();
+    $("div#code-points-dec-message").empty();
     $("div#utf32-message").empty();
 }
 /** Converts text into other representations. */
 function convertFromText() {
     try {
         let codePoints = fromText($("textarea#text").val());
-        $("textarea#code-points").val(toCodePointsHex(codePoints));
+        $("textarea#code-points-hex").val(toCodePointsHex(codePoints));
+        $("textarea#code-points-dec").val(toCodePointsDec(codePoints));
         $("textarea#utf32").val(toUTF32Hex(codePoints));
         clearMessages();
     }
@@ -182,14 +246,33 @@ function convertFromText() {
 /** Converts the hex representation of a code point sequence into other representations. */
 function convertFromCodePointsHex() {
     try {
-        let codePoints = fromCodePointsHex($("textarea#code-points").val());
+        let codePoints = fromCodePointsHex($("textarea#code-points-hex").val());
         $("textarea#text").val(toText(codePoints));
+        $("textarea#code-points-dec").val(toCodePointsDec(codePoints));
         $("textarea#utf32").val(toUTF32Hex(codePoints));
         clearMessages();
     }
     catch (e) {
         if (e instanceof TypeError) {
-            $("div#code-points-message").text("Error: " + e.message);
+            $("div#code-points-hex-message").text("Error: " + e.message);
+        }
+        else {
+            throw e;
+        }
+    }
+}
+/** Converts the decimal representation of a code point sequence into other representations. */
+function convertFromCodePointsDec() {
+    try {
+        let codePoints = fromCodePointsDec($("textarea#code-points-dec").val());
+        $("textarea#text").val(toText(codePoints));
+        $("textarea#code-points-hex").val(toCodePointsHex(codePoints));
+        $("textarea#utf32").val(toUTF32Hex(codePoints));
+        clearMessages();
+    }
+    catch (e) {
+        if (e instanceof TypeError) {
+            $("div#code-points-dec-message").text("Error: " + e.message);
         }
         else {
             throw e;
@@ -201,7 +284,8 @@ function convertFromUTF32Hex() {
     try {
         let codePoints = fromUTF32Hex($("textarea#utf32").val());
         $("textarea#text").val(toText(codePoints));
-        $("textarea#code-points").val(toCodePointsHex(codePoints));
+        $("textarea#code-points-hex").val(toCodePointsHex(codePoints));
+        $("textarea#code-points-dec").val(toCodePointsDec(codePoints));
         clearMessages();
     }
     catch (e) {
@@ -215,6 +299,7 @@ function convertFromUTF32Hex() {
 }
 $(() => {
     $("button#convert-from-text").on("click", convertFromText);
-    $("button#convert-from-code-points").on("click", convertFromCodePointsHex);
+    $("button#convert-from-code-points-hex").on("click", convertFromCodePointsHex);
+    $("button#convert-from-code-points-dec").on("click", convertFromCodePointsDec);
     $("button#convert-from-utf32").on("click", convertFromUTF32Hex);
 });
--- src/unicode_converter/unicode_converter.mts
+++ src/unicode_converter/unicode_converter.mts
@@ -1,29 +1,38 @@
 import $ from "jquery";
 
 const HEX_DIGIT_REGEX = /[0-9a-fA-F]/;
+const DEC_DIGIT_REGEX = /[0-9]/;
 
 /**
  * Throws an error if the given code point isn't valid.
+ * @param decimal Whether to display the code point in decimal in an error message. Defaults to displaying in hex.
  * @throws {TypeError} Thrown when the given code point is:
  * - not an integer,
  * - outside the valid range, or
  * - reserved for a surrogate.
  */
-function validateCodePoint(codePoint: number) {
+function validateCodePoint(codePoint: number, decimal=false) {
     if (!Number.isInteger(codePoint)) {
-        throw new TypeError(`Non-integer code point: ${codePoint}`);
+        throw new TypeError(`Non-integer code point (${codePoint})`);
     }
-    if (codePoint < 0 || codePoint > 0x10FFFF) {
-        let codePointDisplay = Math.abs(codePoint).toString(16).toUpperCase();
+
+    let codePointDisplay;
+    if (decimal) {
+        codePointDisplay = codePoint.toString();
+    } else {
+        codePointDisplay = Math.abs(codePoint).toString(16).toUpperCase();
         if (codePoint >= 0) {
             codePointDisplay = "0x" + codePointDisplay.padStart(4, "0");
         } else {
             codePointDisplay = "-0x" + codePointDisplay;
         }
-        throw new TypeError(`Code point outside valid range: ${codePointDisplay}`);
+    }
+
+    if (codePoint < 0 || codePoint > 0x10FFFF) {
+        throw new TypeError(`Code point outside valid range (${codePointDisplay})`);
     }
     if (codePoint >= 0xD800 && codePoint <= 0xDFFF) {
-        throw new TypeError(`Surrogate code point: 0x${codePoint.toString(16).toUpperCase()}`);
+        throw new TypeError(`Surrogate code point (${codePointDisplay})`);
     }
 }
 
@@ -62,11 +71,10 @@ function fromCodePointsHex(str: string) {
                 partialCodePointHex = "";
             }
         } else if (HEX_DIGIT_REGEX.test(char)) {
-            if (partialCodePointHex.length === 6) {
+            partialCodePointHex += char.toUpperCase();
+            if (partialCodePointHex.length > 6) {
                 throw new TypeError(`Hex digit sequence longer than 6 digits (${partialCodePointHex})`);
             }
-
-            partialCodePointHex += char.toUpperCase();
         } else {
             throw new TypeError(`Encountered a character that is neither a hex digit nor a whitespace (\"${char}\")`);
         }
@@ -75,6 +83,46 @@ function fromCodePointsHex(str: string) {
     if (partialCodePointHex !== "") {
         let codePoint = Number.parseInt(partialCodePointHex, 16);
         validateCodePoint(codePoint);
+        sequence.push(codePoint);
+    }
+
+    return sequence;
+}
+
+/**
+ * Converts the decimal representation of a code point sequence into the code point sequence.
+ * @throws {TypeError} Thrown when the given string:
+ * - contains a character that is neither a decimal digit nor a whitespace,
+ * - contains a decimal digit sequence with length greater than 7 digits,
+ * - contains a code point outside the valid range, or
+ * - contains a code point reserved for an surrogate.
+ */
+function fromCodePointsDec(str: string) {
+    let sequence = [];
+    let partialCodePointDec = ""; // A partially-built code point (in decimal representation)
+
+    for (const char of str) {
+        if (/\s/.test(char)) {
+            if (partialCodePointDec !== "") {
+                let codePoint = Number(partialCodePointDec);
+                validateCodePoint(codePoint, true);
+                sequence.push(codePoint);
+                partialCodePointDec = "";
+            }
+        } else if (DEC_DIGIT_REGEX.test(char)) {
+            partialCodePointDec += char;
+
+            if (partialCodePointDec.length > 7) {
+                throw new TypeError(`Decimal digit sequence longer than 7 digits (${partialCodePointDec})`);
+            }
+        } else {
+            throw new TypeError(`Encountered a character that is neither a decimal digit nor a whitespace (\"${char}\")`);
+        }
+    }
+
+    if (partialCodePointDec !== "") {
+        let codePoint = Number(partialCodePointDec);
+        validateCodePoint(codePoint, true);
         sequence.push(codePoint);
     }
 
@@ -151,6 +199,23 @@ function toCodePointsHex(sequence: number[]) {
 }
 
 /**
+ * Converts a code point sequence to its decimal representation.
+ * @throws {TypeError} Thrown when the given sequence contains a code point that is:
+ * - not an integer,
+ * - outside the valid range, or
+ * - reserved for a surrogate.
+ */
+function toCodePointsDec(sequence: number[]) {
+    let string = "";
+    for (const [i, codePoint] of sequence.entries()) {
+        validateCodePoint(codePoint);
+        if (i > 0) string += " ";
+        string += codePoint.toString();
+    }
+    return string;
+}
+
+/**
  * Converts a code point sequence to UTF-32.
  * @throws {TypeError} Thrown when the given sequence contains a code point that is:
  * - not an integer,
@@ -171,6 +236,7 @@ function toUTF32Hex(sequence: number[]) {
 function clearMessages() {
     $("div#text-message").empty();
     $("div#code-points-hex-message").empty();
+    $("div#code-points-dec-message").empty();
     $("div#utf32-message").empty();
 }
 
@@ -180,6 +246,7 @@ function convertFromText() {
         let codePoints = fromText($("textarea#text").val() as string);
 
         $("textarea#code-points-hex").val(toCodePointsHex(codePoints));
+        $("textarea#code-points-dec").val(toCodePointsDec(codePoints));
         $("textarea#utf32").val(toUTF32Hex(codePoints));
 
         clearMessages();
@@ -198,12 +265,32 @@ function convertFromCodePointsHex() {
         let codePoints = fromCodePointsHex($("textarea#code-points-hex").val() as string);
 
         $("textarea#text").val(toText(codePoints));
+        $("textarea#code-points-dec").val(toCodePointsDec(codePoints));
         $("textarea#utf32").val(toUTF32Hex(codePoints));
 
         clearMessages();
     } catch (e) {
         if (e instanceof TypeError) {
             $("div#code-points-hex-message").text("Error: " + e.message);
+        } else {
+            throw e;
+        }
+    }
+}
+
+/** Converts the decimal representation of a code point sequence into other representations. */
+function convertFromCodePointsDec() {
+    try {
+        let codePoints = fromCodePointsDec($("textarea#code-points-dec").val() as string);
+
+        $("textarea#text").val(toText(codePoints));
+        $("textarea#code-points-hex").val(toCodePointsHex(codePoints));
+        $("textarea#utf32").val(toUTF32Hex(codePoints));
+
+        clearMessages();
+    } catch (e) {
+        if (e instanceof TypeError) {
+            $("div#code-points-dec-message").text("Error: " + e.message);
         } else {
             throw e;
         }
@@ -217,6 +304,7 @@ function convertFromUTF32Hex() {
 
         $("textarea#text").val(toText(codePoints));
         $("textarea#code-points-hex").val(toCodePointsHex(codePoints));
+        $("textarea#code-points-dec").val(toCodePointsDec(codePoints));
 
         clearMessages();
     } catch (e) {
@@ -231,5 +319,6 @@ function convertFromUTF32Hex() {
 $(() => {
     $("button#convert-from-text").on("click", convertFromText);
     $("button#convert-from-code-points-hex").on("click", convertFromCodePointsHex);
+    $("button#convert-from-code-points-dec").on("click", convertFromCodePointsDec);
     $("button#convert-from-utf32").on("click", convertFromUTF32Hex);
 });
