878f24ea1f4ea23d600cb46440266b6c0444378d
NutronStar45 <56185420+NutronStar45@users.noreply.github.com>
2026-06-28 20:48:00 +0800

    feat: separate layout generation into file

--- dist/common.mjs
+++ dist/common.mjs
@@ -1,10 +1,7 @@
 import $ from "jquery";
 import {} from "./alerts.mjs";
 import * as alerts from "./alerts.mjs";
-// The page width at which the navigation is hidden by default
-const NAV_HIDDEN_PAGE_WIDTH = "700px";
-// Whether navigation is forced hidden
-let navForcedHidden = false;
+import { genLayout } from "./layout.mjs";
 /**
  * Creates an element with the SVG namespace.
  * This function is necessary because all SVG elements must be namespaced.
@@ -286,81 +283,7 @@ export function downloadFile(content, filename) {
     $('<a></a>').attr("download", filename).attr("href", url)[0].click();
     URL.revokeObjectURL(url);
 }
-/** Generates the header. */
-function genHeader() {
-    // Header
-    const headerHTML = '<header>'
-        + '<button id="nav-toggle" class="img-button" title="Toggle navigation"><div></div></button>'
-        + '<a href="/">NutronStar45\'s Work</a>'
-        + '</header>';
-    $("div#content-wrapper").before(headerHTML);
-}
-/** Generates the navigation. */
-function genNav() {
-    // Fetch sections
-    const sections = $("main > section").map(function () {
-        const title = $(this).children("h3").text();
-        const id = $(this).attr("id");
-        if (id === undefined) {
-            console.error(`Section with title ${title} doesn't have an ID`);
-        }
-        return { title, id: id ?? "" };
-    }).get();
-    // Navigation
-    let navHTML = '<nav><a href="#">Top</a><hr>'
-        + '<section><h3>Links</h3>'
-        + '<ul><li><a href="/projects.html">Projects</a></li>'
-        + '<li><a href="/math.html">Math</a></li></ul></section>';
-    // Generate section links
-    if (sections.length > 0) {
-        navHTML += '<hr><section><h3>Sections</h3><ul>';
-        for (const section of sections) {
-            navHTML += `<li><a href="#${section.id}">${section.title}</a></li>`;
-        }
-        navHTML += '</ul></section>';
-    }
-    navHTML += '</nav>';
-    $("div#content-wrapper").prepend(navHTML);
-}
-/** Handles navigation visibility. */
-function handleNavVisibility() {
-    $("nav").toggleClass("force-hidden", navForcedHidden);
-    // Handle navigation toggle
-    const smallWidth = matchMedia(`(max-width: ${NAV_HIDDEN_PAGE_WIDTH})`);
-    $("button#nav-toggle").on("click", () => {
-        if (smallWidth.matches) {
-            $("nav").toggleClass("force-hidden", false);
-            $("nav").toggleClass("force-shown");
-            $("div#main-overlay").toggleClass("active");
-            $("div#main-wrapper").prop("inert", !$("div#main-wrapper").prop("inert"));
-            localStorage.setItem("navForcedHidden", String(false));
-        }
-        else {
-            $("nav").toggleClass("force-hidden");
-            localStorage.setItem("navForcedHidden", String($("nav").hasClass("force-hidden")));
-        }
-    });
-    // Handle width change
-    smallWidth.addEventListener("change", () => {
-        $("nav").toggleClass("force-shown", false);
-        $("div#main-overlay").toggleClass("active", false);
-        $("div#main-wrapper").prop("inert", false);
-    });
-    $("div#main-overlay").on("click", function () {
-        $("nav").toggleClass("force-shown", false);
-        $(this).toggleClass("active");
-        $("div#main-wrapper").prop("inert", false);
-    });
-}
-/** Generates the layout. */
-function genLayout() {
-    $("div#content-wrapper").before('<div id="main-overlay"></div>');
-    genHeader();
-    genNav();
-    handleNavVisibility();
-}
 $(() => {
-    navForcedHidden = localStorage.getItem("navForcedHidden") === "true";
     genLayout();
     // Location on 404 page
     // `slice(1)` to trim the beginning slash
---
+++ src/layout.mts
@@ -0,0 +1,94 @@
+import $ from "jquery";
+
+// The page width at which the navigation is hidden by default
+const NAV_HIDDEN_PAGE_WIDTH = "700px";
+
+// Whether navigation is forced hidden
+let navForcedHidden = localStorage.getItem("navForcedHidden") === "true";
+
+
+/** Generates the header. Deletes the header first if already present. */
+export function genHeader() {
+    $("header").remove();
+
+    const headerHTML = '<header>'
+            + '<button id="nav-toggle" class="img-button" title="Toggle navigation"><div></div></button>'
+            + '<a href="/">NutronStar45\'s Work</a>'
+        + '</header>';
+    $("div#content-wrapper").before(headerHTML);
+}
+
+/** Handles navigation visibility. */
+function handleNavVisibility() {
+    $("nav").toggleClass("force-hidden", navForcedHidden);
+
+    // Handle navigation toggle
+    const smallWidth = matchMedia(`(max-width: ${NAV_HIDDEN_PAGE_WIDTH})`);
+    $("button#nav-toggle").on("click", () => {
+        if (smallWidth.matches) {
+            $("nav").toggleClass("force-hidden", false);
+            $("nav").toggleClass("force-shown");
+            $("div#main-overlay").toggleClass("active");
+            $("div#main-wrapper").prop("inert", !$("div#main-wrapper").prop("inert"));
+
+            localStorage.setItem("navForcedHidden", String(false));
+        } else {
+            $("nav").toggleClass("force-hidden");
+            localStorage.setItem("navForcedHidden", String($("nav").hasClass("force-hidden")));
+        }
+    });
+
+    // Handle width change
+    smallWidth.addEventListener("change", () => {
+        $("nav").toggleClass("force-shown", false);
+        $("div#main-overlay").toggleClass("active", false);
+        $("div#main-wrapper").prop("inert", false);
+    })
+
+    $("div#main-overlay").on("click", function () {
+        $("nav").toggleClass("force-shown", false);
+        $(this).toggleClass("active");
+        $("div#main-wrapper").prop("inert", false);
+    });
+}
+
+/** Generates the navigation. Deletes the navigation first if already present. */
+export function genNav() {
+    $("nav").remove();
+
+    // Fetch sections
+    const sections = $("main > section").map(function () {
+        const title = $(this).children("h3").text();
+        const id = $(this).attr("id");
+        if (id === undefined) {
+            console.error(`Section with title ${title} doesn't have an ID`);
+        }
+        return { title, id: id ?? "" };
+    }).get();
+
+    // Navigation
+    let navHTML = '<nav><a href="#">Top</a><hr>'
+        + '<section><h3>Links</h3>'
+        + '<ul><li><a href="/projects.html">Projects</a></li>'
+        + '<li><a href="/math.html">Math</a></li></ul></section>';
+
+    // Generate section links
+    if (sections.length > 0) {
+        navHTML += '<hr><section><h3>Sections</h3><ul>';
+        for (const section of sections) {
+            navHTML += `<li><a href="#${section.id}">${section.title}</a></li>`;
+        }
+        navHTML += '</ul></section>';
+    }
+
+    navHTML += '</nav>';
+    $("div#content-wrapper").prepend(navHTML);
+    handleNavVisibility();
+}
+
+/** Generates the layout. */
+export function genLayout() {
+    $("div#content-wrapper").before('<div id="main-overlay"></div>');
+    genHeader();
+    genNav();
+}
